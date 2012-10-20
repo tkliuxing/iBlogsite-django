a9d54e7f38442f1d80d209f62c635ff2fb320644
Ronald Bai <ouyanghongyu@gmail.com>
2012-10-20 14:05:37 +0800

    Add Profile bar in log page.

--- iblog/context_processors.py
+++ iblog/context_processors.py
@@ -13,26 +13,25 @@ def request(request):
 
 
 def google_analytics(request):
-	return {'GA_CODE': getattr(settings, 'GA_CODE', '')}
+    return {'GA_CODE': getattr(settings, 'GA_CODE', '')}
 
 
 def tag_manager(request):
-	tag_styles = [
-		'',
-		'label-success',
-		'label-warning',
-		'label-important',
-		'label-info',
-		'label-inverse',
-	]
-	return {'tagmanager': Tag.objects, 'tag_styles': tag_styles}
+    tag_styles = [
+        '',
+        'label-success',
+        'label-warning',
+        'label-important',
+        'label-info',
+        'label-inverse',
+    ]
+    return {'tagmanager': Tag.objects, 'tag_styles': tag_styles}
 
 
 def show_profile(request):
-	from homepage.models import UserProfile
-	profile = UserProfile.objects.filter(show_in_page=True)
-	if len(profile) == 1:
-		print profile[0].description
-		return {'USERPROFILE': profile[0].description}
-	else:
-		return {'USERPROFILE': None}
+    from homepage.models import UserProfile
+    profile = UserProfile.objects.filter(show_in_page=True)
+    if len(profile) == 1:
+        return {'USERPROFILE': profile[0].description}
+    else:
+        return {'USERPROFILE': None}
