7650f63ebba24149ee5a0a7919ddfc8c5f596c3c
Ronald Bai <ouyanghongyu@gmail.com>
2013-07-13 16:56:29 +0800

    change wsgi.py add local path.

--- iblog/wsgi.py
+++ iblog/wsgi.py
@@ -14,7 +14,8 @@ framework.
 
 """
 import os
-
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'iblog')))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iblog.settings")
 
 # This application object is used by any WSGI server configured to use this
